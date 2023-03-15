da8a0a346812b1e64f5566e4e376c45e2c75b0f6
cloud-dev-user <70812553+cloud-dev-user@users.noreply.github.com>
2023-03-16 01:35:42 +0530

    Create string_compare.groovy

---
+++ Groovy Strings/string_compare.groovy
@@ -0,0 +1,8 @@
+def firstName = 'John'
+def lastName = 'Doe'
+
+if (firstName == 'John' && lastName == 'Doe') {
+    println "Welcome, John Doe!"
+} else {
+    println "Sorry, you're not John Doe."
+}
