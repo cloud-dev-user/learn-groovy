0eb38e8edc83b02a6b2e8a167e45ae96566613f1
cloud-dev-user <70812553+cloud-dev-user@users.noreply.github.com>
2023-03-16 01:30:37 +0530

    Create switch_statement.groovy

---
+++ Groovy_Decision_making/switch_statement.groovy
@@ -0,0 +1,15 @@
+def day = "Monday"
+
+switch (day) {
+    case "Monday":
+        println "Today is Monday."
+        break
+    case "Tuesday":
+        println "Today is Tuesday."
+        break
+    case "Wednesday":
+        println "Today is Wednesday."
+        break
+    default:
+        println "Not a valid day."
+}
