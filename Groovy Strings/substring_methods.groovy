415581b207caf0594b34e5866ece4366b837e75f
cloud-dev-user <70812553+cloud-dev-user@users.noreply.github.com>
2023-03-16 01:34:56 +0530

    Create substring_methods.groovy

---
+++ Groovy Strings/substring_methods.groovy
@@ -0,0 +1,6 @@
+def message = "   Hello, World!   "
+println message.length()
+println message.toUpperCase()
+println message.toLowerCase()
+println message.trim()
+println message.substring(7, 12))
