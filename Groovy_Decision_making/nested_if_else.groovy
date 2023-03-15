d9b78b243a0facb3478c5cb95da0f6883e847724
cloud-dev-user <70812553+cloud-dev-user@users.noreply.github.com>
2023-03-16 01:29:58 +0530

    Create nested_if_else.groovy

---
+++ Groovy_Decision_making/nested_if_else.groovy
@@ -0,0 +1,12 @@
+def age = 21
+def gender = "male"
+
+if (age >= 18) {
+    if (gender == "male") {
+        println "You are a man."
+    } else {
+        println "You are a woman."
+    }
+} else {
+    println "You are a minor."
+}
