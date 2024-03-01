ea44281c44c9c0f3c911333a3364f206fa51f879
Ubuntu <ubuntu@ip-172-31-22-186.ap-south-1.compute.internal>
2024-03-01 06:03:40 +0000

    updated jenkins installatio nscript

---
+++ install_jenkins.sh
@@ -0,0 +1,18 @@
+# Install JDK  
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre
+java -version
+
+# below script with install Jenkins 
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
+# enable Jenkins 
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
